047e425975281f8e003d7bd9a172e0b7b79a4ed7
desync <unknown@unknown.unknown>
2023-10-25 11:07:37 +0600

    added json config system and live reloading of json config

--- online.py
+++ online.py
@@ -3,10 +3,25 @@ import json
 import time
 import websocket
 import requests
+import hashlib
+
+
+def load_config_file(filename):
+    with open(filename, "r") as f:
+        json_data = json.load(f)
+    return json_data
+
+
+def hash_json(json_data):
+    return hashlib.sha256(json.dumps(json_data).encode("utf-8")).hexdigest()
+
+
+config_data = load_config_file("./config.json")
+config_hash = hash_json(config_data)
 
 userinfo = requests.get(
     "https://discordapp.com/api/v9/users/@me",
-    {"Authorization": token, "Content-Type": "application/json"},
+    {"Authorization": config_data["token"], "Content-Type": "application/json"},
 ).json()
 
 
@@ -16,13 +31,13 @@ userid = userinfo["id"]
 auth = {
     "op": 2,
     "d": {
-        "token": token,
+        "token": config_data["token"],
         "properties": {
             "$os": "Arch Linux",
             "$browser": "Google Chrome",
             "$device": "Arch Linux",
         },
-        "presence": {"status": status, "afk": False},
+        "presence": {"status": config_data["status"], "afk": False},
     },
     "s": None,
     "t": None,
@@ -34,17 +49,17 @@ cstatus = {
         "activities": [
             {
                 "type": 4,
-                "state": custom_status,
+                "state": config_data["custom_status"],
                 "name": "Custom Status",
                 "id": "custom",
                 "emoji": {
-                    "name": emoji_name,
-                    "id": emoji_id,
-                    "animated": isAnimatedEmoji,
+                    "name": config_data["emoji_name"],
+                    "id": config_data["emoji_id"],
+                    "animated": config_data["isAnimatedEmoji"],
                 },
             }
         ],
-        "status": status,
+        "status": config_data["status"],
         "afk": False,
     },
 }
@@ -65,6 +80,18 @@ def keep_online():
 def run_keep_online():
     print(f"staying alive for {username}{discriminator} , user_id = {userid}.")
     while True:
+        config_data = load_config_file("./config.json")
+        if hash_json(config_data) != config_hash:
+            config_hash = hash_json(config_data)
+            status = config_data["status"]
+            cstatus["d"]["activities"][0]["state"] = config_data["status"]
+            cstatus["d"]["activities"][0]["emoji"]["name"] = config_data["emoji_name"]
+            cstatus["d"]["activities"][0]["emoji"]["id"] = config_data["emoji_id"]
+            cstatus["d"]["activities"][0]["emoji"]["animated"] = config_data[
+                "isAnimatedEmoji"
+            ]
+            cstatus["d"]["status"] = config_data["status"]
+            print("Config file updated")
         keep_online(token, status)
         time.sleep(30)
 
